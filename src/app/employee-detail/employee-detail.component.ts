b2e9b31e6fc2bc7cb598bb7507efce6d2d1549a4
Kyle Cordes <kyle.cordes@oasisdigital.com>
2018-04-03 11:13:46 -0500

    curriculum update

--- src/app/employee-detail/employee-detail.component.ts
+++ src/app/employee-detail/employee-detail.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Observable } from 'rxjs/Observable';
+import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 import { Employee } from '../employee-loader.service';
